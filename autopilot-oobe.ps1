d9d5d6ab892ed61576161e727346c8e3918df033
Inditech-AHellyer <111679590+Inditech-AHellyer@users.noreply.github.com>
2024-05-06 20:55:58 +1000

    Create autopilot-oobe.ps1

---
+++ autopilot-oobe.ps1
@@ -0,0 +1,22 @@
+# Set the security protocol
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Bypass the execution policy
+PowerShell.exe -ExecutionPolicy Bypass
+
+# Install the Get-WindowsAutopilotInfo script
+Install-Script -name Get-WindowsAutopilotInfo -Force
+
+# Set the execution policy for the current process
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+
+# Prompt for assignedUser and groupTag
+$assignedUser = Read-Host -Prompt 'Input your assigned user'
+$groupTag = Read-Host -Prompt 'Input your group tag'
+
+# Display the entered values
+Write-Host "Assigned User: $assignedUser"
+Write-Host "Group Tag: $groupTag"
+
+# Run the Get-WindowsAutopilotInfo script with the entered values
+Get-WindowsAutopilotInfo -Online -groupTag $groupTag -assignedUser $assignedUser
