6fd39ab6aff1457d55632eb2173a4acb9e8139b9
Inditech-AHellyer <111679590+Inditech-AHellyer@users.noreply.github.com>
2024-06-21 20:03:41 +1000

    Update and rename autopilot-oobe.ps1 to getwinapinfo.ps1

--- getwinapinfo.ps1
+++ getwinapinfo.ps1
@@ -8,12 +8,12 @@ Install-Script -name Get-WindowsAutopilotInfo -Force
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 
 # Prompt for assignedUser and groupTag
-$assignedUser = Read-Host -Prompt 'Input your assigned user'
-$groupTag = Read-Host -Prompt 'Input your group tag'
+#$assignedUser = Read-Host -Prompt 'Input your assigned user'
+#$groupTag = Read-Host -Prompt 'Input your group tag'
 
 # Display the entered values
-Write-Host "Assigned User: $assignedUser"
-Write-Host "Group Tag: $groupTag"
+#Write-Host "Assigned User: $assignedUser"
+#Write-Host "Group Tag: $groupTag"
 
 # Run the Get-WindowsAutopilotInfo script with the entered values
-Get-WindowsAutopilotInfo -Online -groupTag $groupTag -assignedUser $assignedUser
+# Get-WindowsAutopilotInfo -Online -groupTag $groupTag -assignedUser $assignedUser
